a1aaa211fae182849aeac0c6f9fd83604eb99182
Danny <walshdanny700@gmail.com>
2018-12-08 18:07:57 +0000

    added desction to function walk_pkg_gen

--- plsql_util.py
+++ plsql_util.py
@@ -2,6 +2,9 @@ import os
 
 
 def walk_pkg_gen(root_dir):
+    '''
+      Description: Yields only file extension of pks and pkb
+    '''
     ext_spec = '.pks'
     ext_body = '.pkb'
     for root, _, anyfile in os.walk(root_dir):
