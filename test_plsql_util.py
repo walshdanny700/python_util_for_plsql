e2fbd15e01c0010fe095af1f02254817947734c0
Danny <walshdanny700@gmail.com>
2018-12-09 00:24:05 +0000

    another test

--- test_plsql_util.py
+++ test_plsql_util.py
@@ -10,6 +10,14 @@ def test_walk_pkg_gen():
         assert ext != '.txt'
 
 
+def test_walk_pkg_gen_ext():
+    dir_test = 'test'
+    location = os.path.join(os.path.abspath(__file__), dir_test)
+    for _, cur_file in putil.walk_pkg_gen(location):
+        _, ext = os.path.splitext(cur_file)
+        assert ext in ['.pks', '.pkb']
+
+
 def test_walk_root_gen():
     dir_test = 'test'
     location = os.path.join(os.path.abspath(__file__), dir_test)
