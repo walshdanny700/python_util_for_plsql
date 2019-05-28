2bc133bd5180dfbfdf1128890965bac0b30a759c
Danny <walshdanny700@gmail.com>
2019-05-28 19:06:37 +0100

    fixed security issue with jinja2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.rst", "r") as fh:
 
 setuptools.setup(
     name="plsqlutil",
-    version="0.0.19",
+    version="0.0.20",
     author="Danny Walsh",
     author_email="walshdanny700@gmail.com",
     description="Python util function for plsql",
